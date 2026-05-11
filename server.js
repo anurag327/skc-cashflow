44cefd224fd23aa571a7dc7e878820d3dd8856cd
anurag327 <anurag@skcrealestate.ae>
2026-05-11 23:41:41 +0400

    increase upload size

--- server.js
+++ server.js
@@ -9,7 +9,15 @@ const app = express();
 
 // MIDDLEWARE
 app.use(cors());
-app.use(express.json());
+app.use(express.json({
+  limit:'50mb'
+}));
+
+app.use(express.urlencoded({
+  limit:'50mb',
+  extended:true
+}));
+
 app.use(express.static(__dirname));
 
 // DATABASE
